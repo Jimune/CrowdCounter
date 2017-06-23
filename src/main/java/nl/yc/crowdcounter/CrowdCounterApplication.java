176b82114e8c916dfc42b25d397213096ae11511
Jimune <me@jimune.com>
2017-06-23 15:32:10 +0200

    Initial layout commit

---
+++ src/main/java/nl/yc/crowdcounter/CrowdCounterApplication.java
@@ -0,0 +1,12 @@
+package nl.yc.crowdcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdCounterApplication.class, args);
+	}
+}
