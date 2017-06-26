d99c7b832c11691811848aa6ae46f8b9134ecd71
Jimune <me@jimune.com>
2017-06-26 11:03:13 +0200

    Added SpringSandwich

--- src/main/java/nl/yc/crowdcounter/CrowdCounterApplication.java
+++ src/main/java/nl/yc/crowdcounter/CrowdCounterApplication.java
@@ -2,8 +2,10 @@ package nl.yc.crowdcounter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.kastkode.springsandwich.filter"})
 public class CrowdCounterApplication {
 
 	public static void main(String[] args) {
--- src/main/java/nl/yc/crowdcounter/controller/HomeController.java
+++ src/main/java/nl/yc/crowdcounter/controller/HomeController.java
@@ -1,5 +1,8 @@
 package nl.yc.crowdcounter.controller;
 
+import com.kastkode.springsandwich.filter.annotation.Before;
+import com.kastkode.springsandwich.filter.annotation.BeforeElement;
+import nl.yc.crowdcounter.prehandler.LoginCheckFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/home")
+    @Before(@BeforeElement(LoginCheckFilter.class))
     public String test() {
         return "index";
     }
