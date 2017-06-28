80dcc5af688694a4cb6d3020dbb8868e692abc83
Jimune <me@jimune.com>
2017-06-28 10:48:58 +0200

    First step to modify User

--- src/main/java/nl/yc/crowdcounter/filter/PermissionFilter.java
+++ src/main/java/nl/yc/crowdcounter/filter/PermissionFilter.java
@@ -24,6 +24,7 @@ public class PermissionFilter extends HandlerInterceptorAdapter {
 
     @Autowired
     UserCrudRepo repo;
+    //Iets
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
